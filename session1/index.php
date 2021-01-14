2bef0207cd8679f5d4b71fe1488d44300560acda
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2021-01-14 10:19:41 +0100

    session one php class

---
+++ session1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+   
+<?php 
+
+ $name = "Jude";
+ $age = (float) 30;
+ $studentID = "2123343hdhd";
+ echo $age;
+ $currency = (int) 30.24;
+ echo "<br>$currency";
+
+// nfdsjfdkflskf
+/**
+ * 
+ */
+
+?>
+
+<h1><a href="redirect.php">Click to go to redirect</a></h1>
+
+</body>
+</html>
---
+++ session1/redirect.php
@@ -0,0 +1,4 @@
+<?php 
+header("Location: login.php");
+ echo "<h1>Welcome to redirect</h1>"
+?>
